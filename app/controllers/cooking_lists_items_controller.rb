eafb49601d3c2c573cede3b8e5103055710177ac
Guillaume Grassiant <guillaume.grassiant@gmail.com>
2019-05-27 17:52:37 -0400

    fridge and refacto on cooking list

--- app/controllers/cooking_lists_items_controller.rb
+++ app/controllers/cooking_lists_items_controller.rb
@@ -1,9 +1,9 @@
-class CookingListsItemsController < ApplicationController
-  def show
+class CookingListItemsController < ApplicationController
+  def index
     @cooking_list_items = CookinglistItem.where(user: current_user)
   end
 
-  def add
+  def create
     recipe = Recipe.find(params[:id])
     cooking_list_item = CookingListItem.new
     cooking_list_item.recipe = recipe
--- app/controllers/fridge_items_controller.rb
+++ app/controllers/fridge_items_controller.rb
@@ -1,29 +1,46 @@
 class FridgeItemsController < ApplicationController
-  def show
+  def index
     @fridge_items = FridgeItem.where(user: current_user)
   end
 
-  def add
-
+  def create
+    ingredient = Ingredient.find(params[:id])
+    fridge_item = FridgeItem.new
+    fridge_item.ingredient = ingredient
+    fridge_item.user = current_user
+    fridge_item.save!
   end
 
   def delete
-
+    fridge_item = FridgeItem.find(params[:id])
+    fridge_item.destroy!
   end
 
   def increase
-
+    fridge_item = FridgeItem.find(params[:id])
+    fridge_item.quantity += 1
+    fridge_item.save!
   end
 
   def decrease
-
+    fridge_item = FridgeItem.find(params[:id])
+    fridge_item.quantity -= 1
+    fridge_item.save!
   end
 
   def fill
-
+    @grocery_list_items.each do |ingredient|
+      fridge_item = FridgeItem.new
+      fridge_item.ingredient = ingredient
+      fridge_item.user = current_user
+      fridge_item.save!
+    end
   end
 
   def empty
-
+    @recipe_ingredients.each do |ingredient|
+      fridge_item = FridgeItem.where(ingredient: ingredient)
+      fridge_item.destroy!
+    end
   end
 end
