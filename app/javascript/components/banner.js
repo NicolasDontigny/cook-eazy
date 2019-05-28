77da08a9cddf1ffe90c7ccdf8066ae7d5bc085f2
Michael Zhu <michaelzhu63@hotmail.com>
2019-05-28 16:04:47 -0400

    finalized landing-page logic

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,9 +1,14 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-    new Typed('#banner-typec-text', {
-        strings: ["EAT.", "LOVE." , "SAVE"],
-        typeSpeed: 50,
+    new Typed('#banner-typed-text', {
+        strings: ["EAT.^1600 LOVE.^1600 SAVE."],
+        typeSpeed: 30,
+        fadeOut: true,
+        // backSpeed: 0,
+        // backDelay: 0,
+        startDelay: 1600,
+        cursorChar: "",
         loop: true
     });
 }
