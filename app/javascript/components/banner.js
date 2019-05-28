34cfb7e25f79139b3f155cc5c52b2ef26e0b37e8
Michael Zhu <michaelzhu63@hotmail.com>
2019-05-28 17:14:40 -0400

    typed.js debugged

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,18 +1,21 @@
 import Typed from 'typed.js';
+import { end } from 'worker-farm';
 
-const xxx = document.getElementById('banner-typed-text');
+const debugTyped = document.getElementById('banner-typed-text');
 
 const loadDynamicBannerText = () => {
-    new Typed('#banner-typed-text', {
-        strings: ["EAT.^1600 LOVE.^1600 SAVE.^1600"],
-        typeSpeed: 30,
-        fadeOut: true,
-        backSpeed: 0,
-        backDelay: 0,
-        startDelay: 1600,
-        cursorChar: "",
-        loop: true
-    });
-}
+    if (debugTyped) {
+        new Typed(debugTyped, {
+            strings: ["EAT.^1600 LOVE.^1600 SAVE.^1600"],
+            typeSpeed: 30,
+            fadeOut: true,
+            backSpeed: 0,
+            backDelay: 0,
+            startDelay: 1600,
+            cursorChar: "",
+            loop: true
+        });
+    };
+};
 
-export { loadDynamicBannerText }
+export { loadDynamicBannerText };
