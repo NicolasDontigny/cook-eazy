11ba7ff8f2f33b5c0fac966043ad5c22fa80d72f
Guillaume Grassiant <guillaume.grassiant@gmail.com>
2019-05-28 17:04:14 -0400

    saving ratings v1

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,12 @@ class Recipe < ApplicationRecord
 
   has_many :cooking_list_items
   has_many :users, through: :cooking_list_items
+
+  def update_ratings!
+    all_ratings = []
+    self.reviews.each do |review|
+      all_ratings << review.rating
+    end
+    self.rating = all_ratings.reduce(:+) / all_ratings.size.to_f
+  end
 end
