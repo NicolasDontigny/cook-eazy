69091a102392163aed09f819d99041bf91090a8c
Michael Zhu <michaelzhu63@hotmail.com>
2019-05-29 14:46:54 -0400

    avatar uoloader finalized logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    def avatar_for(user)
+        @avatar = user.avatar
+        if @avatar.nil?
+            "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+        else
+            if !@avatar.url
+                "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+            else
+                cl_image_path @avatar, width: 150, height: 150, crop: :thumb, gravity: :face
+            end
+        end
+    end
 end
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,8 +1,5 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
-
-  def default_url(avatar)
-    "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  end
+  
   # Remove everything else
 end
