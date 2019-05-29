9cadfecb29f6a712ea25db1b0bbae5e952786800
Michael Zhu <michaelzhu63@hotmail.com>
2019-05-29 11:52:37 -0400

    added field for avatar in new.html.erb and edit.html.erb

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,5 +1,8 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
+  def default_url(avatar)
+    "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
   # Remove everything else
 end
