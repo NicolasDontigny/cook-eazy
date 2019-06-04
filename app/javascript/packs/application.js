d440cb9ee610a1da8c7983e6f1e30b0f378406d4
Guillaume Grassiant <guillaume.grassiant@gmail.com>
2019-06-03 21:29:44 -0400

    solving merging conflicts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ import { stepsCarousel } from '../plugins/steps_carousel'
 import { clickOnRecipeCard } from '../plugins/add_to_wishlist'
 import { initSelect2 } from '../plugins/init_select2';
 import { clickRightOrLeft } from '../plugins/progress_bar';
+import { addEventToHearts } from '../plugins/heart_icon';
 import 'select2/dist/css/select2.css';
 
 // import { recipePopup } from '../plugins/recipe_popup'
@@ -29,5 +30,7 @@ wishlistCarousel();
 stepsCarousel();
 
 clickRightOrLeft();
+
+addEventToHearts();
 
 // clickOnRecipeCard();
