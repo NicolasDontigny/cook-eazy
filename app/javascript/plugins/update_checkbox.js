643f372266102737e0ef5f0e809a31373acd1e85
Guillaume Grassiant <guillaume.grassiant@gmail.com>
2019-06-05 18:07:18 -0400

    saving checkbox

--- app/javascript/plugins/update_checkbox.js
+++ app/javascript/plugins/update_checkbox.js
@@ -1,16 +1,28 @@
 const updateCheckbox = (empty) => {
   console.log(empty)
-  const checkbox = document.querySelector(`a[data='checkbox']`);
-  if (checkbox) {
+  const checkboxes = document.querySelectorAll(`a[data='checkbox']`);
+  if (checkboxes) {
     if (empty) {
       // checkbox.insertAdjacentHTML("afterend", checkboxHTML);
-      checkbox.style.visibility = "hidden";
+      checkboxes.forEach(checkbox => {
+        checkbox.style.visibility = "hidden";
+          }
+        );
+      // checkbox.style.visibility = "hidden";
     }
     else {
-      checkbox.style.visibility = "visible";
-      // console.log(checkbox.firstChild);
-      checkbox.firstChild.nextElementSibling.classList.remove('fa-check-square');
-      checkbox.firstChild.nextElementSibling.classList.add('fa-square')
+
+      checkboxes.forEach(checkbox => {
+        checkbox.style.visibility = "visible";
+        // console.log(checkbox.firstChild);
+        checkbox.firstChild.nextElementSibling.classList.remove('fa-check-square');
+        checkbox.firstChild.nextElementSibling.classList.add('fa-square')
+          }
+        );
+      // checkbox.style.visibility = "visible";
+      // // console.log(checkbox.firstChild);
+      // checkbox.firstChild.nextElementSibling.classList.remove('fa-check-square');
+      // checkbox.firstChild.nextElementSibling.classList.add('fa-square')
     }
   }
 }
